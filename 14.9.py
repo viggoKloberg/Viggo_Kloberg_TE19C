6f1bc972d486f95ce8023ff5c0d59fc99f01d3d5
viggoKloberg <70135100+viggoKloberg@users.noreply.github.com>
2020-09-14 12:13:26 +0200

    if

--- 14.9.py
+++ 14.9.py
@@ -1,1 +1,11 @@
-vinkel = float(input("ange en vinkel v i grader, där 0° < v < 360° "))
+vinkel = float(input("ange en vinkel v i grader, där 0° < v < 360°: "))
+
+if vinkel < 90 and vinkel > 0:
+    print("spetsig")
+elif vinkel == 90:
+    print("rät")
+elif vinkel > 90 and vinkel < 180:
+    print("trubbig")
+else:
+    print("varken eller")
+    
