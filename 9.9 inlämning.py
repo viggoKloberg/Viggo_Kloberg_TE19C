dab860c444fd7ca56e3a2ed0527e7510cb373c4e
viggoKloberg <70135100+viggoKloberg@users.noreply.github.com>
2020-09-21 10:34:20 +0200

    9/9 inlämning

--- 9.9 inlämning.py
+++ 9.9 inlämning.py
@@ -1,3 +1,6 @@
 gånger = float(input("hur många gånger har du åkt buss denna månaden?"))
 kostnad = gånger*30
-print(f"det kostar {kostnad}kr")
+if kostnad > 775:
+    print(f"Det kostar {kostnad}kr, det är värt det att köpa månadskort")
+else:
+    print(f"Det kostar {kostnad}kr, det är inte värt det att köpa månadskort")
